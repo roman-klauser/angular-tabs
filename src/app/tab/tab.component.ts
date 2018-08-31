e8b72c4058344c1868379a014a78feb3a418902d
Roman Klauser <romanklauser@Romans-MacBook-Pro-2.local>
2018-08-31 15:03:06 +0200

    add some content

--- src/app/tab/tab.component.ts
+++ src/app/tab/tab.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-tab',
@@ -7,7 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TabComponent implements OnInit {
 
-  constructor() { }
+  @Input()
+  title: string;
+
+  constructor() {
+  }
 
   ngOnInit() {
   }
