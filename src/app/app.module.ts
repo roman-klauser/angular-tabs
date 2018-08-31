7b404ac65b69c4b75f8d4e3691ae51c0ec0f5416
Roman Klauser <romanklauser@Romans-MacBook-Pro-2.local>
2018-08-31 14:14:09 +0200

    add empty components: tab-panel and tab

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TabPanelComponent } from './tab-panel/tab-panel.component';
+import { TabComponent } from './tab/tab.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TabPanelComponent,
+    TabComponent
   ],
   imports: [
     BrowserModule
